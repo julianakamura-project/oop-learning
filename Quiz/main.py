4d85a9e2637a943d5b4e649ffa812259d91b0e62
julianakamura-project <julia.nakamura.work@gmail.com>
2026-09-23 00:23:40 -0300

    code: added main body of quiz

--- Quiz/main.py
+++ Quiz/main.py
@@ -1,9 +1,20 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
+
 for question in question_data:
     question_text = question["text"]
     question_answer = question["answer"]
     new_question = Question(text = question_text, answer = question_answer)
     question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.questions_remain():
+    quiz.next_question()
+
+print("You've completed the quiz.\n"
+      f"Your final score: {quiz.score}/{quiz.question_number}")
+quiz.tally_score()
